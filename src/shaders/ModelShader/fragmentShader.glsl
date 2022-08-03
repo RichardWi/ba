7e787d19e5962591dff5493f824294d56d3cb7c5
RichardWi <richard.wilschewsky@gmx.de>
2022-08-03 17:36:29 +0200

    removed unneeded data

--- src/shaders/ModelShader/fragmentShader.glsl
+++ src/shaders/ModelShader/fragmentShader.glsl
@@ -1,6 +1,6 @@
 precision mediump float;
 
-uniform sampler2D uTexture;
+//uniform sampler2D uTexture; not needed, but could be used as shown below
 uniform bool uNdviColorPalette;
 uniform bool uHeatColorPalette;
 uniform bool uChange;
@@ -9,57 +9,51 @@ uniform sampler2D uHeatColorMap;
 
 varying float vCurrentColor;
 varying float vPastColor;
-varying vec2 vUv;
-varying vec3  vPosition;
-
-
+//varying vec2 vUv; not needed, but could be used as shown below
 
 
   void main()
     { 
       if (uChange){
-      if(vCurrentColor < vPastColor)
-      { 
-        
-       gl_FragColor=vec4(1.0,0.0,0.0,1.0);
 
-       
-      }
-      if(vCurrentColor > vPastColor ){
-          gl_FragColor=vec4(0.0,1.0,0.0,1.0);
-         
-      }
-      if(vCurrentColor== vPastColor){
-          gl_FragColor=vec4(1.0,1.0,1.0,0.2);
-         
-      }
+        if(vCurrentColor < vPastColor)
+        { 
+          gl_FragColor=vec4(1.0,0.0,0.0,1.0);
+        }
+
+        if(vCurrentColor > vPastColor )
+        {
+          gl_FragColor=vec4(0.0,1.0,0.0,1.0);       
+        }
+
+        if(vCurrentColor== vPastColor)
+        {     
+          gl_FragColor=vec4(1.0,1.0,1.0,0.2);      
+        }
       }
 
       if(uNdviColorPalette)
-      {
-        vec4 textureNDVI = texture2D(uTexture, vUv);
-    float u = (textureNDVI.r * (600.0 - 1.0) + 0.5) / 600.0;
-     
-  gl_FragColor = texture2D(uNDVIColorMap, vec2(u, 0.5));
-      }
+        {
+        //vec4 textureNDVI = texture2D(uTexture, vUv); alternate calculation using the texture
+        //would need to pass textureNDVI.r instead of vColorPassed in the next line
+          float u = (vCurrentColor * (600.0 - 1.0) + 0.5) / 600.0;
+          gl_FragColor = texture2D(uNDVIColorMap, vec2(u, 0.5));
+        }
+
       if(uHeatColorPalette)
-      {
-       
+        {
+        //vec4 textureHeat= texture2D(uTexture,vUv); alternate calculation using the texture
+        //would need to pass textureHeat.r instead of vColorPassed in the next line        
+          float u = (vCurrentColor  * (600.0 - 1.0) + 0.5) / 600.0;
+          gl_FragColor = texture2D(uHeatColorMap, vec2(u, 0.5));
+        }
 
-              vec4 textureHeat= texture2D(uTexture,vUv);
-               
-    float u = (textureHeat.g * (600.0 - 1.0) + 0.5) / 600.0;
-     
-  gl_FragColor = texture2D(uHeatColorMap, vec2(u, 0.5));
-      }
-      if(uNdviColorPalette == false && uHeatColorPalette == false && uChange == false) {
-      vec4 textureC = texture2D(uTexture,vUv);
-      textureC.a=1.0;
-    
-       gl_FragColor=textureC;
-      }
+      if(uNdviColorPalette == false && uHeatColorPalette == false && uChange == false) 
+        {
+          //vec4 textureC = texture2D(uTexture,vUv); 
+          //textureC.a=1.0;
+          //gl_FragColor=textureC; alternate calculation using the texture
+          gl_FragColor=vec4( vCurrentColor , vCurrentColor , vCurrentColor ,1.0);
+        }
 
-   
-     
-     
-      }
+    }
--- src/shaders/ModelShader/vertexShader.glsl
+++ src/shaders/ModelShader/vertexShader.glsl
@@ -2,23 +2,19 @@
 uniform mat4 projectionMatrix;
 uniform mat4 viewMatrix;
 uniform mat4 modelMatrix;
-uniform float uFrequencyG;
-uniform vec4 uColor;
+
 
 
 attribute vec3 position;
-attribute vec2 uv;
-attribute vec4 color;
-attribute vec3 normal;
+//attribute vec2 uv; not needed, but could be in fragment shader, fragment shader still has the code to use it
 attribute float pastColor;
 attribute float currentColor;
 
-varying vec2 vUv;
-varying vec3 vPosition;
+//varying vec2 vUv; not needed, but could be in fragment shader, fragment shader still has the code to use it
 varying vec4 vColor;
-varying vec3 vNormal;
 varying float  vCurrentColor;
 varying float vPastColor;
+
 
 
 void main()
@@ -28,12 +24,16 @@ void main()
     vec4 projectionPosition = projectionMatrix * viewPosition;
     gl_Position = projectionPosition;
 
-    vUv = uv;
-    vPosition = position;
-    vColor = color;
-    vNormal = normal;
-    vPastColor = pastColor;
-    vCurrentColor = currentColor;
+    //vUv = uv; not needed, but could be in fragment shader, fragment shader still has the code to use it
+    
+    //current/past Color (0-255) has to be converted to 0-1
+    //mulitply by 0.0039215 to get the value in 0-1
+    //multiply could be faster than divide
+    //is done here for every vertex, could also be done in fragment shader for every pixel
+    //is fast as long as the number of vertices is lower than the number of pixels
+    //could be slower for different models and on different screens
+    vPastColor = pastColor * 0.0039215;
+    vCurrentColor = currentColor * 0.0039215;
     
 
 }
